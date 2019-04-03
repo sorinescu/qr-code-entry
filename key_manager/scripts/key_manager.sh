df0f6d6e23719ab89651cb02b65901e331013c76
Sorin Otescu <sorinu@gmail.com>
2019-04-03 13:16:31 +0300

    Add FreeBSD rc.d scripts

--- key_manager/scripts/key_manager.sh
+++ key_manager/scripts/key_manager.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 HOME_DIR=/home/piqr
 INSTALL_DIR=${HOME_DIR}/key_manager
 
 export KEY_MANAGER_CONFIG=${INSTALL_DIR}/config.cfg
 export FLASK_APP=app
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-source ${HOME_DIR}/pfsense36/bin/activate
+. ${HOME_DIR}/pfsense36/bin/activate
 
 cd ${INSTALL_DIR}
 exec flask run --host 0.0.0.0
